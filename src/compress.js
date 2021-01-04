ec70991bdd8cf2d8e5aa73203238d9ae53a60c74
hos1818 <69805385+hos1818@users.noreply.github.com>
2021-01-04 03:48:10 +0200

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -44,33 +44,16 @@ function compress(req, res, input) {
     
   }else{
 
-    const image = sharp(input);
-    
-    image
-        .metadata(function(err, metadata){
-            let pixelCount = metadata.width * metadata.height;
-            var compressionQuality = req.params.quality;
-            
-            if(pixelCount > 3000000 || metadata.size > 1536000){
-                compressionQuality *= 0.1
-            }else if(pixelCount > 2000000 && metadata.size > 1024000){
-                compressionQuality *= 0.25
-            }else if(pixelCount > 1000000 && metadata.size > 512000){
-                compressionQuality *= 0.5
-            }else if(pixelCount > 500000 && metadata.size > 256000){
-                compressionQuality *= 0.75
-            }
-            compressionQuality = Math.ceil(compressionQuality)
-            
-            sharp(input)
-            .grayscale(req.params.grayscale)
-            .toFormat(format, {
-                quality: compressionQuality,
-                progressive: true,
-                optimizeScans: true
-            })
-            .toBuffer((err, output, info) => {
-                if (err || !info || res.headersSent) return redirect(req, res)
+      sharp(input)
+    .grayscale(req.params.grayscale)
+    .toFormat(format, {
+      quality: req.params.quality,
+      progressive: true,
+      optimizeScans: true
+    })
+    .toBuffer((err, output, info) => {
+      if (err || !info || res.headersSent) return redirect(req, res)
+
 
                 setResponseHeaders(info, format)
                 res.write(output)
