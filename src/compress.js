74fddd40db5eb28cd6702dc19106d839b0621883
hos1818 <69805385+hos1818@users.noreply.github.com>
2021-01-25 03:13:10 +0200

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -82,8 +82,6 @@ function compress(req, res, input) {
     function setResponseHeaders (info, imgFormat){
         res.setHeader('content-type', `image/${imgFormat}`)
         res.setHeader('content-length', info.size)
-        let filename = (new URL(req.params.url).pathname.split('/').pop() || "image") + '.' + format
-        res.setHeader('Content-Disposition', 'inline; filename="' + filename + '"' )
         res.setHeader('x-original-size', req.params.originSize)
         res.setHeader('x-bytes-saved', req.params.originSize - info.size)
         res.status(200)
