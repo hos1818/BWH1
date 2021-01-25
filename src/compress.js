9fa76810aa641aa2da78a4aceb3d986588a52d28
hos1818 <69805385+hos1818@users.noreply.github.com>
2021-01-25 03:20:33 +0200

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -5,7 +5,6 @@ const {execFile} = require('child_process')
 const gif2webp = require('gif2webp-bin')
 const fs = require('fs')
 const os = require('os')
-const {URL} = require('url')
 
 function compress(req, res, input) {
   const format = req.params.webp ? 'webp' : 'jpeg'
